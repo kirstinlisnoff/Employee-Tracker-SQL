b2763d1ae06c25e176edee2b3358e6dd372412d6
Kiki <kirstinlisnoff5@gmail.com>
2025-03-05 19:02:06 -0500

    Fixed typos

---
+++ Unsolved/db/schema.sql
@@ -0,0 +1,25 @@
+-- Active: 1740443733571@@127.0.0.1@5432@employees_db
+DROP DATABASE IF EXISTS employees_db;
+CREATE DATABASE employees_db;
+\c employees_db;
+
+CREATE TABLE department (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(30) UNIQUE NOT NULL
+);
+CREATE TABLE role (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(30) UNIQUE NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INTEGER NOT NULL,
+    Foreign Key (department_id) REFERENCES department(id) ON DELETE SET NULL
+);
+CREATE TABLE employee (
+    id SERIAL PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INTEGER NOT NULL,
+    manager_id INTEGER,
+    Foreign Key (role_id) REFERENCES role(id) ON DELETE SET NULL,
+    Foreign Key (manager_id) REFERENCES employee(id) ON DELETE SET NULL
+);
